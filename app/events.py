5478532177cd3f5409fbafa9c2ba641240e0c14f
jgr68 <john.ryan@rutgers.edu>
2014-06-12 00:16:15 -0400

    Admin panel now works.

--- app/events.py
+++ app/events.py
@@ -5,16 +5,49 @@ import MySQLdb
 def add_event(title, timeDate, placeDesc, placeURL, description):
 
 	db = db_connect()
-	cursor = db.cursor()
+	crosshairs = db.cursor()
 
 	query = "insert into events (title, timeDate, placeDesc, placeURL, description)"
 	query += " values ( '%s', '%s', '%s', '%s', '%s');" % (title, timeDate, placeDesc, placeURL, description)	
 	if app.debug == True:
 		print query
-	
-	cursor.execute(query)
-	db.commit()
-	return		
 
-	
-	
+	try:	
+		crosshairs.execute(query)
+		db.commit()
+	except:
+		db.rollback()
+
+	db.close()
+	return
+
+def get_events():
+
+	db = db_connect()
+	crosshairs = db.cursor()
+
+	query = "select * from events order by id desc limit 10;"
+
+	if app.debug == True:
+		print query
+
+	try:	
+		crosshairs.execute(query)
+		db.commit()
+	except:
+		db.rollback()
+
+	eventList = []
+	for row in crosshairs:
+		eventList.append({'title' : row[1],\
+					'timeDate' : row[2],\
+					'placeDesc' : row[3],\
+					'placeURL' : row[4],\
+					'description' : row[5] })
+
+	if app.debug == True:
+		for test in eventList:
+			print test
+	db.close()
+	return eventList
+
--- app/views.py
+++ app/views.py
@@ -5,7 +5,7 @@ from app import app
 
 #internal
 from form import EventForm
-from events import add_event
+from events import add_event, get_events
 
 # ----------------------------------------------------------
 @app.route('/')
@@ -36,39 +36,12 @@ def admin():
 			
 @app.route('/events')
 def events():
-	upcoming = [ # fake array of posts
-		{
-			'heading': "D&D Club : Monster Mash",
-			'date_locale': "Friday - May 2nd @ BCC International Lounge",
-			'body' :  "Come for free pizza and drinks as we celebrate the imminent\
-				release of D&D Next! We're hosting an awesome Pathfinder\
-				 tournament where players will lead parties of monsters \
-				into combat! Make sure to check out the special ruleset for\
-				the tournament by clicking\
-				and to let us know if you have any questions."
-					
-		},
-		{
-			'heading': "Come join us!",
-			'date_locale': "Friday - August 9th @ Werblin (Busch Campus)",
-			'body' : 'Vivamus faucibus bibendum mi, non pretium est ultrices et.\
-                                   Donec risus mauris, laoreet eget pretium sed, cursus sed\
-                                   nunc. Morbi porta lacus ut condimentum ultricies. Nam\
-                                   ligula lorem, tempor et elit id, ullamcorper vestibulum mauris.'
-		}
-	]
-	weekly = [ # fake array of posts
-		{
-			'heading': "Monthly meetup!",
-			'date_locale': "5pm, first Friday of every month @ Busch Campus Center",
-			'body' : 'Come by to see what we have to offer, find a group to play\
-					with, and get a quick game in!'}
-	]
+
+	eventList = get_events()	
 
 	return render_template("events.html",
 					title = 'Events',
-					weekly = weekly,
-					upcoming = upcoming)
+					eventList = eventList)
 
 @app.route('/subscribe')
 def subscribe():
